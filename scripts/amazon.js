0e04b53fb06fa45d8b5b8c5bf0ec5474e6c5f3e0
Suraj Shah <surajshah.bombay@gmail.com>
2024-08-21 00:54:17 +0530

    Used Modules and imported cart from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import { cart } from '../data/cart.js';
+
 let productsHTML = '';
 
 products.map(value => {
@@ -46,10 +48,45 @@ products.map(value => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${value.id}">
             Add to Cart
           </button>
         </div>`
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+let cartQuantity = 0;
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            const productId = button.dataset.productId;
+
+            let matchingItem;
+
+            cart.forEach((item) => {
+                if(productId === item.productId) {
+                    matchingItem = item;
+                }
+            })
+
+            if(matchingItem) {
+                matchingItem.quantity += 1;
+                cartQuantity += 1;
+            }
+            else{
+                cart.push({productId: productId,
+                    quantity: 1,
+                });
+                cartQuantity += 1;
+            }
+            console.log(cart);
+
+            document.querySelector('.cart-quantity')
+                .innerHTML = cartQuantity;
+        }
+    );
+    })
+
+
