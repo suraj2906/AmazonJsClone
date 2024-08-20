6c4d480560a83b49977cded453c86320c04d784f
Suraj Shah <surajshah.bombay@gmail.com>
2024-08-21 00:15:51 +0530

    Used products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,3 @@
-
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87,
-    },
-    price: 1090,
-},
-{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4.0,
-        count: 127,
-    },
-    price: 2059,
-},
-{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56,
-    },
-    price: 799,
-}];
-
 let productsHTML = '';
 
 products.map(value => {
@@ -50,7 +21,7 @@ products.map(value => {
           </div>
 
           <div class="product-price">
-            $${(value.price / 100).toFixed(2)}
+            $${(value.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
