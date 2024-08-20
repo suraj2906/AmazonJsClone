301472a22fc8cbc535219bbbe638e3c6b01dc8f5
Suraj Shah <surajshah.bombay@gmail.com>
2024-08-21 01:05:20 +0530

    Moved AddToCart to cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from '../data/cart.js';
+import { cart, addToCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 
 let productsHTML = '';
@@ -57,35 +57,20 @@ products.map(value => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-let cartQuantity = 0;
+function updateCartQuantity() {
+    let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    }),
+    document.querySelector('.cart-quantity').innerHTML = cartQuantity;
+}
 
 document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
         button.addEventListener('click', () => {
             const productId = button.dataset.productId;
-
-            let matchingItem;
-
-            cart.forEach((item) => {
-                if(productId === item.productId) {
-                    matchingItem = item;
-                }
-            })
-
-            if(matchingItem) {
-                matchingItem.quantity += 1;
-                cartQuantity += 1;
-            }
-            else{
-                cart.push({productId: productId,
-                    quantity: 1,
-                });
-                cartQuantity += 1;
-            }
-            console.log(cart);
-
-            document.querySelector('.cart-quantity')
-                .innerHTML = cartQuantity;
+            addToCart(productId);
+            updateCartQuantity();
         }
     );
     })
